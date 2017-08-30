c1ab291f044ca83a2440771faaa34b741c3b095e
Khushali Mehta <khushalimehta49@gmail.com>
2017-08-30 15:45:36 -0500

    Ionic Reddit
    Ionic 2 Applications that uses Reddit API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,37 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { HttpModule } from '@angular/http';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { RedditsPage } from '../pages/reddits/reddits';
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { DetailsPage } from '../pages/details/details';
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    RedditsPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    RedditsPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   providers: [
